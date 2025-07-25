8c78de073e701336bff72463dd6a8d07b8234309
yasir5405 <yasirnaseem5415@gmail.com>
2025-07-25 16:57:28 +0530

    finish login page ui

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -62,10 +62,13 @@ const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
 
   return (
-    <div className="h-dvh w-full flex">
+    <div
+      className="min-h-dvh md:h-dvh w-full flex"
+      style={{ fontFamily: "Geist Sans, sans-serif " }}
+    >
       {/* Left Div/Form */}
       <main className="h-full w-full md:w-[50%] flex flex-col md:pl-10 md:pr-44 md:py-8 py-6 px-3">
-        <div className="h-full w-full">
+        <div className="h-full w-full flex flex-col gap-1 md:gap-3">
           {/* Logo Div */}
           <div className="w-full py-5 gap-5 flex flex-col justify-between items-start">
             <Link
@@ -152,13 +155,38 @@ const Login = () => {
           </form>
 
           {/* BorderDiv */}
-          <div className="w-full flex items-center gap-2 py-2">
+          <div className="w-full flex items-center gap-4 py-2">
             <div className="h-[0.5px] flex-1 bg-neutral-400"></div>
-            <p className="text-neutral-600 text-sm">Or continue with</p>
+            <p className="text-neutral-400 text-sm">Or continue with</p>
             <div className="h-[0.5px] flex-1 w-full bg-neutral-400"></div>
           </div>
 
-          <Button text="Google" className="w-full" />
+          {/* Login with google button */}
+          <Button
+            text="Google"
+            variant="secondary"
+            className="w-full border-blue-500"
+            startIcon={
+              <svg
+                className="h-5 w-5"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+                  fill="currentColor"
+                />
+              </svg>
+            }
+          />
+
+          <p className="text-center text-xs mt-5 md:mt-0 mb-5 md:mb-0 md:text-sm text-neutral-700">
+            By clicking on sign in, you agree to our{" "}
+            <Link className="text-neutral-500" to={"/login"}>
+              Terms of Service
+            </Link>{" "}
+            and Privacy Policy
+          </p>
         </div>
       </main>
 
